8e62929f7d3142df0d60e8098e4af696edf801a1
August-IE <Aeromosele9@gmail.com>
2023-10-24 03:48:10 -0700

    Heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+void swap_ints(int *x, int *y);
+void sift_down(int *array, size_t size, size_t root, size_t end);
+void heap_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
+void swap_ints(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * sift_down - Perform a sift-down operation to maintain the heap property.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ * @root: Index of the root node.
+ * @base: Index of the last node in the heap.
+ */
+void sift_down(int *array, size_t size, size_t base, size_t root)
+{
+	size_t left, right, large;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	large = root;
+
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
+
+	if (large != root)
+	{
+		swap_ints(array + root, array + large);
+		print_array(array, size);
+		sift_down(array, size, base, large);
+	}
+}
+
+/**
+ * heap_sort - Sort an array of integers in ascending
+ *             order using the heap sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Implements the sift-down heap sort
+ * algorithm. Prints the array after each swap.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		sift_down(array, size, size, i);
+
+	for (i = size - 1; i > 0; i--)
+	{
+		swap_ints(array, array + i);
+		print_array(array, size);
+		sift_down(array, size, i, 0);
+	}
+}
